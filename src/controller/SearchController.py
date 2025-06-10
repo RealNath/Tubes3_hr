7016778a8b2f56d39716236344b2e6a0a68dafb9
Ilman <dzubyan04@gmail.com>
2025-06-10 16:15:50 +0700

    chore : restructured files

---
+++ src/controller/SearchController.py
@@ -0,0 +1,6 @@
+from src.models import *
+from src.views.main_menu import MainMenu
+import re
+
+def cv_search(keywords, mode, result_amount):
+    pass
--- src/views/main_menu.py
+++ src/views/main_menu.py
@@ -1,5 +1,7 @@
 from src.generated.main_menu_ui import Ui_MainWindow
+from src.generated.result_card import Ui_resultCard 
 #import src.utils
+from src.controller.SearchController import *
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import re
 
@@ -14,11 +16,18 @@ class MainMenu(QMainWindow):
 
     def get_search_result(self):
         keywords = re.split(r",\s*", self.ui.keywordSearch.toPlainText())
-        mode = "KMP" if self.modeToggle.value() == 0 else "BP"
-        result_amount = self.topMatchesNumber.value()
-        self.search(keywords, mode, result_amount)
+        mode = "KMP" if self.ui.modeToggle.value() == 0 else "BM"
+        result_amount = self.ui.topMatchesNumber.value()
+        self.cv_search(keywords, mode, result_amount)
 
-
-    def search(keywords, mode, result_amount):
+    def renderResult(results, keywords):
         pass
+        #Results should be array of dictionary, 
+        #Dictionary have : Name, total search, occurence for each keyowrd (summary, link) <- later
+        #Put data in label below "Results"
+        #Get top matches number
+        #For each, make a result car, with the data from searches
+        #Put it in grid, 3x3 
+
+    
 
