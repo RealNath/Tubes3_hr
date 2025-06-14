6775e57403a703d5365bf4db8d54faa8cf1577c5
Nathan <therealnathanmc7383@gmail.com>
2025-06-14 23:38:27 +0700

    refactor: renamed parameter's names for consistency

--- src/utils/levensthein.py
+++ src/utils/levensthein.py
@@ -1,22 +1,22 @@
-def levenshtein_distance(str1, str2):
+def levenshtein_distance(text, pattern):
     """
     Fuzzy matching menggunakan algoritma Levenshtein Distance.
     """
-    str_len_1 = len(str1)
-    str_len_2 = len(str2)
-    if str_len_1 == 0: return str_len_2
-    if str_len_2 == 0: return str_len_1
+    text_len = len(text)
+    pattern_len = len(pattern)
+    if text_len == 0: return pattern_len
+    if pattern_len == 0: return text_len
 
     # === Tabel DP ===
         # Inisialisasi
-    dp = [[0] * (str_len_2 + 1) for _ in range(str_len_1 + 1)]
-    for i in range(str_len_1 + 1): dp[i][0] = i
-    for j in range(str_len_2 + 1): dp[0][j] = j
+    dp = [[0] * (pattern_len + 1) for _ in range(text_len + 1)]
+    for i in range(text_len + 1): dp[i][0] = i
+    for j in range(pattern_len + 1): dp[0][j] = j
 
         # Isi tabel
-    for i in range(1, str_len_1 + 1):
-        for j in range(1, str_len_2 + 1):
-            if str1[i-1] == str2[j-1]: cost = 0
+    for i in range(1, text_len + 1):
+        for j in range(1, pattern_len + 1):
+            if text[i-1] == pattern[j-1]: cost = 0
             else: cost = 1
             dp[i][j] = min(
                 dp[i-1][j] + 1, # hapus karakter
@@ -24,14 +24,14 @@ def levenshtein_distance(str1, str2):
                 dp[i-1][j-1] + cost # ganti karakter
             )
     
-    return dp[str_len_1][str_len_2]
+    return dp[text_len][pattern_len]
 
-def similarity_score(str1, str2):
+def similarity_score(text, pattern):
     """
     Skor kemiripan (0.0 sampai 1.0) antara string str1 dan str2.
     """
-    distance = levenshtein_distance(str1, str2)
-    max_length = max(len(str1), len(str2))
+    distance = levenshtein_distance(text, pattern)
+    max_length = max(len(text), len(pattern))
     if max_length == 0: return 1.0
     return 1.0 - distance / max_length
 
