ae0c97700bd5959d365a3ccf893608071c1eae6c
Nathan <therealnathanmc7383@gmail.com>
2025-06-14 23:51:19 +0700

    refactor: gave better example

--- src/utils/levensthein.py
+++ src/utils/levensthein.py
@@ -1,22 +1,22 @@
-def levenshtein_distance(text, pattern):
+def levenshtein_distance(str1, str2):
     """
-    Fuzzy matching menggunakan algoritma Levenshtein Distance.
+    Menghitung Levenshtein *distance* antara string `text` dan `pattern`
     """
-    text_len = len(text)
-    pattern_len = len(pattern)
-    if text_len == 0: return pattern_len
-    if pattern_len == 0: return text_len
+    str_len_1 = len(str1)
+    str_len_2 = len(str2)
+    if str_len_1 == 0: return str_len_2
+    if str_len_2 == 0: return str_len_1
 
     # === Tabel DP ===
         # Inisialisasi
-    dp = [[0] * (pattern_len + 1) for _ in range(text_len + 1)]
-    for i in range(text_len + 1): dp[i][0] = i
-    for j in range(pattern_len + 1): dp[0][j] = j
+    dp = [[0] * (str_len_2 + 1) for _ in range(str_len_1 + 1)]
+    for i in range(str_len_1 + 1): dp[i][0] = i
+    for j in range(str_len_2 + 1): dp[0][j] = j
 
         # Isi tabel
-    for i in range(1, text_len + 1):
-        for j in range(1, pattern_len + 1):
-            if text[i-1] == pattern[j-1]: cost = 0
+    for i in range(1, str_len_1 + 1):
+        for j in range(1, str_len_2 + 1):
+            if str1[i-1] == str2[j-1]: cost = 0
             else: cost = 1
             dp[i][j] = min(
                 dp[i-1][j] + 1, # hapus karakter
@@ -24,20 +24,20 @@ def levenshtein_distance(text, pattern):
                 dp[i-1][j-1] + cost # ganti karakter
             )
     
-    return dp[text_len][pattern_len]
+    return dp[str_len_1][str_len_2]
 
-def similarity_score(text, pattern):
+def similarity_score(str1, str2):
     """
-    Skor kemiripan (0.0 sampai 1.0) antara string str1 dan str2.
+    Skor kemiripan (0.0 sampai 1.0) antara string `text` dan `pattern`.
     """
-    distance = levenshtein_distance(text, pattern)
-    max_length = max(len(text), len(pattern))
+    distance = levenshtein_distance(str1, str2)
+    max_length = max(len(str1), len(str2))
     if max_length == 0: return 1.0
     return 1.0 - distance / max_length
 
 if __name__ == "__main__":
-    a = "oiai oiiai"
-    b = "oiiai oiai"
-    print(f"str1: \"{a}\"\nstr2: \"{b}\"")
-    print(f"Distance: {levenshtein_distance(a, b)}")
-    print(f"Similarity: {similarity_score(a, b):.3f}")
+    text_1 = "never gonna run around in desert typhoons"
+    text_2 = "never gonna run around with dessert spoons"
+    print(f"Teks 1: \"{text_1}\"\nTeks 2: \"{text_2}\"")
+    print(f"Distance: {levenshtein_distance(text_1, text_2)}")
+    print(f"Similarity: {similarity_score(text_1, text_2):.3f}")
